bdf69c669949a4d4a58ff6d41f748a5d0f83994d
Darron Brown <DarronBrown88@outlook.com>
2018-04-14 08:18:59 -0600

    added insert method to ordered array class

--- src/OrdArray.java
+++ src/OrdArray.java
@@ -33,6 +33,24 @@ public class OrdArray {
         }
     }
 
+    public void insert( long value ) {
+
+        int j;
+
+        for ( j = 0; j < nElems; j++ ) {
+            if ( a[j] > value) {
+                break;
+            }
+        }
+
+        for ( int k = j; k < nElems; k++ ) {
+            a[k] = a[k - 1];
+        }
+        a[j] = value;
+        nElems++;
+
+    }
+
     public boolean delete( long value ) {
         int j = find(value);
         if ( j == nElems ) {
