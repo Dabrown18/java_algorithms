0306a39aac9529447a4ad96dad26655a798f0c72
Darron Brown <DarronBrown88@outlook.com>
2018-04-14 22:05:43 -0600

    create person class

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         int maxSize = 100;
-        OrdArray arr;
+        ClassDataArray arr;
+        arr = new ClassDataArray(maxSize);
 
-        arr = new OrdArray(maxSize);
-
-        arr.insert(77);
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(0);
-        arr.insert(66);
-        arr.insert(33);
-
-        int searchKey = 55;
-
-        if ( arr.find(searchKey) != arr.size() ) {
-            System.out.println( "Found " + searchKey );
-        } else {
-            System.out.println( "Cant find " + searchKey );
-        }
-
-        arr.display();
+        arr
 
     }
 }
